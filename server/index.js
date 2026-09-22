a7a84195ff84dac53ba90bffccd99b4cb4242d22
HafizIkrashSE <hafizikrash809@gmail.com>
2026-09-22 15:34:26 +0500

    index.js rewrite

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const UserModel = require('./models/Users');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("../models/Users");
 
 const app = express();
 
@@ -14,21 +14,21 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Atlas connected successfully"))
   .catch(err => console.log("MongoDB connection error:", err));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   UserModel.find({})
     .then(users => res.json(users))
     .catch(err => res.json(err));
 });
 
-app.get('/getUser/:id', (req, res) => {
+app.get("/getUser/:id", (req, res) => {
   const id = req.params.id;
 
   UserModel.findById(id)
-    .then(users => res.json(users))
+    .then(user => res.json(user))
     .catch(err => res.json(err));
 });
 
-app.put('/updateUser/:id', (req, res) => {
+app.put("/updateUser/:id", (req, res) => {
   const id = req.params.id;
 
   UserModel.findByIdAndUpdate(
@@ -37,13 +37,14 @@ app.put('/updateUser/:id', (req, res) => {
       name: req.body.name,
       email: req.body.email,
       age: req.body.age
-    }
+    },
+    { new: true }
   )
-    .then(users => res.json(users))
+    .then(user => res.json(user))
     .catch(err => res.json(err));
 });
 
-app.delete('/deleteUser/:id', (req, res) => {
+app.delete("/deleteUser/:id", (req, res) => {
   const id = req.params.id;
 
   UserModel.findByIdAndDelete({ _id: id })
@@ -53,10 +54,8 @@ app.delete('/deleteUser/:id', (req, res) => {
 
 app.post("/createUser", (req, res) => {
   UserModel.create(req.body)
-    .then(users => res.json(users))
+    .then(user => res.json(user))
     .catch(err => res.json(err));
 });
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
-});
+module.exports = app;
